30b8730edfc96cbc801ba7ee39506709a1266d8f
Emre kavcakar <kavcakaremre@gmail.com>
2023-06-19 11:11:22 -0400

    search bar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
        <Grid item xs={12} md={4}>
           <List />
        </Grid>
-          <Grid>
+          <Grid item xs={12} md={8}>
              <Map />
           </Grid>
        
