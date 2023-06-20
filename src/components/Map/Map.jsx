a7a550e190f94548fcb031e43d98d1748dd50b4f
Emre kavcakar <kavcakaremre@gmail.com>
2023-06-20 16:04:00 -0400

    list

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
 
-export default Map
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyARUpZWR_QHGaX1HZ6OwEtE6g0b1gnnd28" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
